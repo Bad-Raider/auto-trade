5a022e55757990fc9fd1f29fac2f81cba035f596
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-11-10 16:05:01 +0200

    finish auto hero padding style

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ window.addEventListener('load', function () {
     const heroWrapper = document.querySelector('.hero-wraper');
     const viewportHeight = window.innerHeight;
     const viewportWidth = window.innerWidth;
+    const widthTablet = 744;
+    const widthDesktop = 1160;
+    const maxHeightDesktopHero = 760;
     const maxHeightTabletHero = 1133;
     const maxHeightMobileHero = 812;
+    const totalDesktopElement = 494;
     const totalTabletElement = 561;
     const totalMobileElement = 248;
 
-    if (viewportWidth >= 744) {
+    if (viewportWidth >= widthDesktop) {
+        heroWrapper.style.padding = addDesktopHeroTopAndBottomPadding(
+            maxHeightDesktopHero,
+            totalDesktopElement
+        );
+    } else if (viewportWidth >= widthTablet) {
         if (viewportHeight <= maxHeightTabletHero) {
             heroWrapper.style.padding = addHeroTopAndBottomPadding(
                 viewportHeight,
@@ -46,4 +55,12 @@ const addHeroTopAndBottomPadding = (viewportHeight, totalElements) => {
         (viewportHeight - totalElements) / 2 +
         'px'
     );
+};
+
+const addDesktopHeroTopAndBottomPadding = (viewportHeight, totalElements) => {
+    const remnant = viewportHeight - totalElements;
+    const topPadding = (remnant * 61) / 100;
+    const bottomPadding = remnant - topPadding;
+
+    return topPadding + 'px 0 ' + bottomPadding + 'px';
 };
