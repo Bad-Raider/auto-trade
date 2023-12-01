0119d1be92f594326f6c6b0e7ca4e03baf156d54
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-12-01 04:29:27 +0200

    add mobil qualities section

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 import './js/components/btnBackToTop.js';
 // Show full text in inspection section
 import './js/components/inspection.js';
-// hero on all display 
-import './js/components/heroOnAllDisplay.js'
+// hero on all display
+import './js/components/heroOnAllDisplay.js';
+// qualities gallery counter and buttons logic
+import './js/components/qualitiesGallery.js';
---
+++ src/js/components/qualitiesGallery.js
@@ -0,0 +1,34 @@
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const qualitiesList = document.querySelector('.qualities-list');
+
+const width = 322;
+let position = 0;
+
+prevButton.onclick = function () {
+    if (position === -322) {
+        prevButton.style.display = 'none';
+    }
+
+    if (position >= 966) {
+        nextButton.style.display = 'none';
+    }
+    if (position < -966) {
+        nextButton.style.display = 'block';
+    }
+
+    position += width;
+    qualitiesList.style.marginLeft = position + 'px';
+};
+
+nextButton.onclick = function () {
+    if (position <= 0) {
+        prevButton.style.display = 'block';
+    }
+    if (position === -966) {
+        nextButton.style.display = 'none';
+    }
+
+    position -= width;
+    qualitiesList.style.marginLeft = position + 'px';
+};
