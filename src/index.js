f69e8205ff43add84404d3c4f32151e5dc0396c2
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-12-09 12:58:35 +0200

    update style qualities

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './js/components/btnBackToTop.js';
 import './js/components/inspection.js';
 // hero on all display
 import './js/components/heroOnAllDisplay.js';
-// qualities gallery counter and buttons logic
-// import './js/components/qualitiesGallery.js';
 // swiper on page
 import './js/components/swiper.js';
