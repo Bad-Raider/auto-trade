76b62d7ead29a4c367ebfa2e2ea044900ff7d84b
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-12-16 14:53:04 +0200

    fix-bag section stages
    when svg pushes the mobile container

--- src/js/components/sectionStages.js
+++
@@ -1,7 +0,0 @@
-const icon = document.querySelector('.stages-list__icon use');
-
-if (window.innerWidth <= 744) {
-    icon.setAttribute('href', './images/small-symbol-defs.svg#icon-vector-mobile');
-} else {
-    icon.setAttribute('href', './images/large-symbol-defs.svg#icon-vector-tablet');
-}
