4561fb28388e683cecde8689a5e731f063af38f7
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-12-16 16:25:11 +0200

    new functionality
    add plus and minus logic in section-inspection

--- src/js/components/inspection.js
+++ src/js/components/inspection.js
@@ -1,15 +1,25 @@
-const inspectionBtns = document.querySelectorAll(".inspection-btn");
-const inspectionTexts = document.querySelectorAll(".inspection-text");
+const inspectionBtns = document.querySelectorAll(".inspection-discription__btn");
+const inspectionTexts = document.querySelectorAll(".inspection-discription__text");
+const crossSvg = document.querySelectorAll(".inspection-svg__cross");
+const minusSvg = document.querySelectorAll(".inspection-svg__minus");
 
 inspectionBtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     const textElement = inspectionTexts[index];
+    const cross = crossSvg[index];
+    const minus = minusSvg[index];
+
     const isExpanded = textElement.classList.contains("expanded");
 
     if (isExpanded) {
       textElement.classList.remove("expanded");
+      cross.style.display = 'block';
+      minus.style.display = 'none'
+
     } else {
       textElement.classList.add("expanded");
+      cross.style.display = 'none';
+      minus.style.display = 'block'
     }
   });
 });
