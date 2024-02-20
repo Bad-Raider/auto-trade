3c26a537302d140f8c81ea372886ac39aa22a402
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2024-02-20 13:31:10 +0200

    Update allTextForFaqSection.js

--- src/js/components/allTextForFaqSection.js
+++ src/js/components/allTextForFaqSection.js
@@ -1,27 +1,23 @@
 const faqIconBtn = document.querySelectorAll('.faq-icon');
-console.log(faqIconBtn);
 const faqItemText = document.querySelectorAll('.faq-item__text');
 const faqItem = document.querySelectorAll('.faq-item');
 
-
-faqIconBtn.forEach((iconBtn, index) => {
+for (let i = 0; i < faqIconBtn.length; i++) {
+    const iconBtn = faqIconBtn[i];
     iconBtn.addEventListener('click', () => {
-        textElement = faqItemText[index];
-        faqItemElement = faqItem[index];
+        const textElement = faqItemText[i];
+        const faqItemElement = faqItem[i];
         const isExpanded = textElement.classList.contains('expanded');
 
         if (isExpanded) {
             textElement.classList.remove('expanded');
             textElement.style.display = 'none';
-            
             iconBtn.style.transform = 'rotate(0deg)';
-
         } else {
             textElement.classList.add('expanded');
             textElement.style.display = 'block';
             faqItemElement.style.display = 'block';
-
             iconBtn.style.transform = 'rotate(180deg)';
         }
     });
-});
+}
